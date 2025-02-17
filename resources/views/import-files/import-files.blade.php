45701f53b71aa1697858877a44fad75bdae6635f
harold <sinadjanharold22@gmail.com>
2025-02-17 14:51:58 +0800

    update opacity on modal

--- resources/views/import-files/import-files.blade.php
+++ resources/views/import-files/import-files.blade.php
@@ -140,8 +140,6 @@
                                         @input.debounce.500ms="getFileDataPerFile(fileId, 1)"
                                         class="px-4 py-2 text-sm border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-100"
                                     />
-
-
                                 </div>
                             </div>
 
