77dac56dce7307bd6a4e5c97032610b419ef1876
harold <sinadjanharold22@gmail.com>
2025-03-03 13:45:13 +0800

    add validation on ca amount

--- app/Http/Requests/CashAdvanceRequest.php
+++ app/Http/Requests/CashAdvanceRequest.php
@@ -35,7 +35,13 @@ class CashAdvanceRequest extends FormRequest
             'position' => 'required|string|max:255',
             'station' => 'required|string|max:255',
             'check_number' => 'required|string|max:255',
-            'cash_advance_amount' => 'required|numeric|min:0.01',
+            'cash_advance_amount' => [
+                'required',
+                'numeric',
+                'min:0.01',
+                'max:75000000', 
+            ],
+
             'cash_advance_date' => 'required|date|date_format:Y-m-d|before_or_equal:today',
             'dv_number' => 'required|string|max:255',
             'ors_burs_number' => 'required|string|max:255',
@@ -55,6 +61,7 @@ class CashAdvanceRequest extends FormRequest
             'cash_advance_amount.required' => 'Cash Advance Amount is required!',
             'cash_advance_amount.numeric' => 'Cash Advance Amount must be a valid number!',
             'cash_advance_date.required' => 'Cash Advance Date is required!',
+            'cash_advance_amount.max' => 'Cash Advance Amount must not exceed 75 million!',
             'special_disbursing_officer.unique' => 'Special Disbursing officer already exist',
         ];
     }
