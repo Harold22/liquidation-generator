72b7b4bd163e97e8f78f8e8f117e3156528617e6
harold <sinadjanharold22@gmail.com>
2025-05-16 11:31:43 +0800

    update seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,23 +1,22 @@
 <?php
 
-namespace Database\Seeders;
-
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */ public function run(): void
+    public function run()
     {
-        // Create a default admin user
-        User::create([
-            'name' => 'Test',
-            'email' => 'test@gmail.com',
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
             'password' => Hash::make('dswd12345'),
+            'is_active' => true,  
         ]);
+
+        $user->assignRole('Admin');
     }
 }
+
+
